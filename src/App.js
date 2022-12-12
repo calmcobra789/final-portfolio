c5602569e077eff48b59b2d3605946b3b3aab5f5
Suyash Kothari <suyashkothari@suyashs-mbp.devices.brown.edu>
2022-12-12 17:31:01 -0500

    initial draft

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import Grid from '@mui/material/Grid'
+import DisplayCard from './components/DisplayCard';
+
+import portfolio_data from "./assets/portfolio_data.json";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Welcome to My Portfolio
       </header>
+
+        <div className = "Body-Content">
+        <Grid container spacing={2}>
+          <Grid item xs={8}>
+            <DisplayCard item = {portfolio_data[0]}/>
+
+          </Grid>
+          <Grid item xs={4}>
+            <DisplayCard item = {portfolio_data[1]}/>
+          </Grid>
+          <Grid item xs={4}>
+            <DisplayCard item = {portfolio_data[2]}/> 
+          </Grid>
+          <Grid item xs={8}>
+            <DisplayCard item = {portfolio_data[3]}/>
+          </Grid>
+        </Grid>
+        </div>
+
+
+     
     </div>
   );
 }
---
+++ src/components/DisplayCard.jsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import CardMedia from '@mui/material/CardMedia';
+
+
+export default function DisplayCard(props) {
+  const item = props.item;
+  console.log(item);
+
+
+  return (
+    
+    <div className = "DisplayCard">
+    <Card sx={{ minWidth: 275 }} variant="outlined">
+      <CardMedia
+        component="img"
+        height="194"
+        image={item.image}
+        alt={item.name}
+      />  
+      <CardContent>
+      {/* <h1>{item.price}</h1> */}
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          {item.date}
+          
+          
+
+        </Typography>
+        <Typography variant="h5" component="div">
+          {/* be{bull}nev{bull}o{bull}lent */}
+          {item.name}
+
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          {item.organization}
+        </Typography>
+        <Typography variant="body2">
+          {item.descrpiton}
+          <br />
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small" onClick={console.log("clicked 1")}> Learn More </Button>
+      </CardActions>
+    </Card>
+    </div>
+  );
+}
